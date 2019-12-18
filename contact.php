9f482f89f22ae5b4f97b83e0f11f8de6090ac11a
Busari Abdullah <abdullah_busari@yahoo.com>
2019-12-18 17:16:17 +0100

    contact backend

--- contact.php
+++ contact.php
@@ -1,18 +1,44 @@
 <?php
-    $name = $_POST['name'];
-    $mail = $_POST['mail'];
-    $message = $_POST['message'];
 
-    $email_from = 'abdullah_busari@yahoo.com';
-    $email_subject = "Contact";
-    $email_body = "User: $name.\n".
-                    "User_mail: $mail.\n".
-                        "User_message: $message.\n";
+require_once 'source/db_connect.php';
 
-    $to = 'abdullah_busari@yahoo.com';
-    $headers = "From: $email_from \r\n";
-    $headers .= "Reply-To: $mail \r\n";
-    mail($to, $email_subject, $email_body, $headers);
-    header("Location: index.html")
+if(isset($_POST['contact'])) {
+
+      $fullname = 'a';#$_POST['fullname'];
+      $email = 'b';#$_POST['email'];
+      $phonenumber = 'c';#$_POST['phonenumber'];
+      $company = 'd';#$_POST['company'];
+      $country = 'e';#$_POST['country'];
+      $budget = 1000;#$_POST['budget'];
+      $interest = 'Interest'; #$_POST['interest'];
+      $requirements = 'that';#$_POST['requirements'];
+      
+    try {
+      $SQLInsert = "INSERT INTO contacts (fullname, email, phonenumber, company,
+                    country, budget, interest, requirements, sent_at)
+                   VALUES (:fullname, :email, :phonenumber, :company,
+                    :country, :budget, :interest, :requirements, now())";
+
+      $statement = $conn->prepare($SQLInsert);
+      $statement->execute(array(
+        ':fullname' => $fullname, 
+        ':email' => $email, 
+        ':phonenumber' => $phonenumber,
+        ':company' => $company,
+        ':country' => $country,
+        ':budget' => $budget,
+        ':interest' => $interest, 
+        ':requirements' => $requirements
+      ));
+
+      if($statement->rowCount() == 1) {
+          echo "<script> alert('Contact sent');window.location='index.html' </script>";
+      }
+    }
+    catch (PDOException $e) {
+      echo "Error: " . $e->getMessage();
+    }
+
+}
 
 ?>
